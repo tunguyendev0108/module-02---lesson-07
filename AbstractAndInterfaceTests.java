3c36ef5e50b61fe16f85935239c1741efd9392a8
tunguyendev0108 <nguyenhoangtu1898@gmail.com>
2023-10-19 14:33:34 +0700

    Add Practice

---
+++ AbstractAndInterfaceTests.java
@@ -0,0 +1,20 @@
+import animal.Animal;
+import animal.Tiger;
+import animal.Chicken;
+import edible.Edible;
+
+public class AbstractAndInterfaceTests {
+    public static void main(String[] args) {
+        Animal[] animals = new Animal[2];
+        animals[0] = new Tiger();
+        animals[1] = new Chicken();
+        for (Animal animal : animals){
+            System.out.println(animal.makeSound());
+
+            if (animal instanceof Chicken){
+                Edible edibler = (Chicken) animal;
+                System.out.println(edibler.howToEat());
+            }
+        }
+    }
+}
